2865d30fb6756d3502a91269da9aa407d41f5f72
Documenter.jl <documenter@juliadocs.github.io>
2022-06-30 13:59:03 +0000

    build based on a62b432

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ApodizationFunctions","category":"page"},{"location":"#ApodizationFunctions","page":"Home","title":"ApodizationFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ApodizationFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ApodizationFunctions]","category":"page"}]
+}
